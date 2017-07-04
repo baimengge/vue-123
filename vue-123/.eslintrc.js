453be7a077580915afc9b148a764c9979411605b
baimengge <baimengge@rongyitech.com>
2017-07-04 14:34:24 +0800

    api接口列表渲染

--- vue-123/.eslintrc.js
+++ vue-123/.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'no-mixed-spaces-and-tabs': 0,
     'no-tabs':0,
     'indent':0,
+    'space-before-function-paren': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
--- vue-123/src/page/index.vue
+++ vue-123/src/page/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    index
+  <div class="index">
+    <h1 class="title">选择恐惧症的福音！教你认清MVC，MVP和MVVM</h1>
+    <ul>
+    	<li v-for="item in lists">{{item.title}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+	export default{
+		data() {
+			return {
+				lists: []
+			}
+		},
+		created() {
+			// 组件创建完后获取数据，这个就是vue生命周期的创建完成周期
+			this.get_data();
+		},
+		methods: {
+			get_data: function (params) {
+				if (!params) params = {}
+				// 我们这里用全局绑定的$api,方法来获取数据
+				this.$api.get('topics',params,r=>{
+					this.lists = r.data;
+					console.log(r.data);
+				})
+			}
+		}
+	}
+</script>
+<style lang="scss">
+	@import './../style/scss/index'
+</style>
